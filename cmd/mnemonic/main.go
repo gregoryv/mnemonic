d1134d34c8e8c082f705efb45630caa695238cf0
Gregory Vincic <gregoryvse@gmail.com>
2022-12-04 07:41:46 +0100

    Remove versioning from cmd/mnemonic

--- cmd/mnemonic/main.go
+++ cmd/mnemonic/main.go
@@ -4,18 +4,14 @@ import (
 	"flag"
 	"os"
 
-	"github.com/gregoryv/stamp"
 	"xwing.7de.se/mnemonic"
 )
 
-//go:generate stamp -go build_stamp.go -clfile ../../changelog.md
 func main() {
 	me := &mnemonic.MainEntryPoint{}
 	me.InitFlags()
-	stamp.InitFlags()
 	flag.Usage = me.Usage(os.Stderr)
 	flag.Parse()
-	stamp.AsFlagged()
 	me.Enter()
 	me.Exit()
 }
